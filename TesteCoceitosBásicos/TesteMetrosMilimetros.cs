03a1d0394d9b3082958b25a0beab9a4a9673813c
cocacolademais <lilicegray@gmail.com>
2025-09-02 20:55:43 -0300

    teste metros Teste soma

---
+++ TesteCoceitosBásicos/TesteMetrosMilimetros.cs
@@ -0,0 +1,31 @@
+﻿using ConceitosBasicos.uteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteCoceitosBásicos
+{
+    [TestClass]
+    public class TesteMetrosMilimetros
+    {
+
+        [TestMethod]
+        public void TesteConverter()
+        {
+            //cenário
+            int metros = 2;
+            int milimetros;
+
+
+            //ação
+            milimetros = MetrosMilimetros.Converter(metros);
+
+            // verificação
+            Assert.AreEqual(2000, milimetros);
+        }
+
+    }
+}
+
---
+++ TesteCoceitosBásicos/TesteSomaDoisNumeros.cs
@@ -0,0 +1,37 @@
+﻿using ConceitosBasicos.uteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteCoceitosBásicos
+{
+    [TestClass]
+    public class TesteSomaDoisNumeros
+    {
+
+
+
+        [TestMethod]
+        public void TesteSomar(object somador)
+        {
+            //cenário
+            int num1 = 20;
+            int num2 = 22;
+            int resultado;
+            SomaDoisNumeros somador = new SomaDoisNumeros();
+
+
+            //ação
+            resultado = somador.Somar(num1, num2);
+
+            // verificação
+            Assert.are
+        }
+
+    }
+}
+
+
+
